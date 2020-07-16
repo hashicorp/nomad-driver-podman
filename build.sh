886b570ab4991958881a91da827706b755529576
James Rasell <jrasell@hashicorp.com>
2020-07-16 16:17:58 +0200

    deps: update GitHub action used Golang version to 1.14.5.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ mkdir -p build
 mkdir -p build/test
 
 # ensure to build in a isolated GOPATH in order to get predictable dependencies
-export GOPATH=$project/build 
+export GOPATH=$project/build
 
 go install github.com/varlink/go/cmd/varlink-go-interface-generator
 go install gotest.tools/gotestsum
