55d442078d9f32fad07fc9b2a46e7feb7acfa66f
Thomas Weber <towe75@googlemail.com>
2021-01-24 14:15:42 +0100

    Initial Exec implementation

---
+++ api/exec_resize.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func (c *API) ExecResize(ctx context.Context, execId string, height int, width int) error {
+
+	res, err := c.Post(ctx, fmt.Sprintf("/v1.0.0/libpod/exec/%s/resize?h=%d&w=%d", execId, height, width), nil)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusCreated {
+		body, _ := ioutil.ReadAll(res.Body)
+		return fmt.Errorf("unknown error, status code: %d: %s", res.StatusCode, body)
+	}
+
+	return err
+}
