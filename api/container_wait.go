bf21e157fc5124fbce0a44cae2f6a608b6cd466f
Thomas Weber <towe75@users.noreply.github.com>
2021-02-25 07:30:52 +0100

    Improve container_start to handle short living containers (#91)

--- api/container_wait.go
+++ api/container_wait.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // ContainerWait waits on a container to met a given condition
-func (c *API) ContainerWait(ctx context.Context, name string, condition string) error {
+func (c *API) ContainerWait(ctx context.Context, name string, conditions []string) error {
 
-	res, err := c.Post(ctx, fmt.Sprintf("/v1.0.0/libpod/containers/%s/wait?condition=%s", name, condition), nil)
+	res, err := c.Post(ctx, fmt.Sprintf("/v1.0.0/libpod/containers/%s/wait?condition=%s", name, strings.Join(conditions, "&condition=")), nil)
 	if err != nil {
 		return err
 	}
