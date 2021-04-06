c94515a39e42dd98ad120e286aecd1c53af0d439
Pablo Correa Gómez <ablocorrea@hotmail.com>
2021-04-06 23:20:59 +0200

    Fix errors missed while pulling images
    On the /images/pull endpoint, podman's StatusOK response does
    not warranty that the image was actually pulled, just that
    the request was well formatted and no InteralServerError happened.
    If any error happens during the download, the error is written to
    the output stream in the error field.
    
    This error could be identified in the test from the previous commit

--- api/image_pull.go
+++ api/image_pull.go
@@ -2,7 +2,10 @@ package api
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 )
@@ -16,10 +19,23 @@ func (c *API) ImagePull(ctx context.Context, nameWithTag string) error {
 	}
 
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
 	if res.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(res.Body)
 		return fmt.Errorf("unknown error, status code: %d: %s", res.StatusCode, body)
+	}
+
+	dec := json.NewDecoder(res.Body)
+	var report ImagePullReport
+	for {
+		if err = dec.Decode(&report); err == io.EOF {
+			break
+		} else if err != nil {
+			return fmt.Errorf("Error reading response: %w", err)
+		}
+
+		if report.Error != "" {
+			return errors.New(report.Error)
+		}
 	}
 
 	return nil
